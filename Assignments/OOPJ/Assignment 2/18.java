7ee1a4a6997c3df16a66fafad2d139e2bf0fa6ca
kushal12111 <kushalsehrawat999@gmail.com>
2025-03-21 00:52:04 +0530

    Update 18.java

---
+++ Assignments/OOPJ/Assignment 2/18.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the student's percentage: ");
+        double percentage = scanner.nextDouble();
+        String result = (percentage >= 40) ? "Pass" : "Fail";
+        System.out.println("The student has: " + result);
+    }
+}
