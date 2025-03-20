758f7ebb2467a875da8f94c30f460032c3dcc7bd
kushal12111 <kushalsehrawat999@gmail.com>
2025-03-21 00:02:18 +0530

    Add files via upload

---
+++ Assignments/Logic Building/Assignment 1/Code/First.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class First{
+
+      public static void main(String args[]){
+      
+      Scanner sc=new Scanner(System.in);
+      System.out.println("enter a number a");
+      int a=sc.nextInt();
+
+      if(a>=0){
+      System.out.println("the number is positive");
+      }
+      else{
+      System.out.println("the number is not positive");
+      }
+
+
+}
+
+
+
+
+}
---
+++ Assignments/Logic Building/Assignment 1/Code/Fourth.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Fourth{
+
+      public static void main(String args[]){
+      
+      Scanner sc=new Scanner(System.in);
+      System.out.println("enter the time t");
+      int t=sc.nextInt();
+
+      if(t>5 && t<12){
+      System.out.println("Good Morning");
+      }
+      else{
+      System.out.println("no response");
+      }
+
+
+}
+
+
+
+
+}
